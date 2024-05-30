0bbc7bd1ef44546a30cdf7ad28c52365653843b2
MrChikip <mrchikip@users.noreply.github.com>
2024-05-30 15:16:56 -0500

    delete task

--- Server/controllers/task.controllers.js
+++ Server/controllers/task.controllers.js
@@ -1,42 +1,47 @@
 import { pool } from "../db.js";
 
 export const getTasks = async (req, res) => {
-    const [result] = await pool.query(
-        "SELECT * FROM tasks ORDER BY createdAt ASC"
-    );
-    res.json(result);
+  const [result] = await pool.query(
+    "SELECT * FROM tasks ORDER BY createdAt ASC"
+  );
+  res.json(result);
 };
 
 export const getTask = async (req, res) => {
-    const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
-        req.params.id,
-    ]);
+  const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]);
 
-    if (result.length === 0) {
-        return res.status(404).json({
-            error: "Task not found",
-        });
-    }
-    res.json(result[0]);
+  if (result.length === 0) {
+    return res.status(404).json({
+      error: "Task not found",
+    });
+  }
+  res.json(result[0]);
 };
 
 export const createTask = async (req, res) => {
-    const { title, description } = req.body;
-    const [result] = await pool.query(
-        "INSERT INTO tasks (title, description) VALUES (?, ?)",
-        [title, description]
-    );
-    res.json({
-        id: result.insertId,
-        title,
-        description,
-    });
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    "INSERT INTO tasks (title, description) VALUES (?, ?)",
+    [title, description]
+  );
+  res.json({
+    id: result.insertId,
+    title,
+    description,
+  });
 };
 
 export const updateTask = (req, res) => {
-    res.send("actualizando tarea");
+  res.send("actualizando tarea");
 };
 
-export const deleteTask = (req, res) => {
-    res.send("eliminando tarea");
+export const deleteTask = async (req, res) => {
+  const [result] = pool.query("DELETE FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]);
+  if (result.affectedRows === 0)
+    return res.status(404).json({ message: "Task not found" });
+  return res.sendStatus(204);
 };
