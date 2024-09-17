315a1931cc1af4ede0fffb77befd59765d31e058
MrChikip <mrchikip@users.noreply.github.com>
2024-09-17 14:40:59 -0500

    correccion de errores

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import TaskCard from "../components/TaskCard";
 import { useTasks } from "../context/TaskProvider";
